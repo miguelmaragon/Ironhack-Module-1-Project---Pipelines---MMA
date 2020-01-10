ea5c2ecbaba4956fd8de2de59c3d45359f44b60c
miguelmaragon <mmaragon@gmail.com>
2020-01-10 20:11:15 +0100

    finished the proyect

--- package1/report.py
+++ package1/report.py
@@ -1,10 +1,12 @@
-import pandas as pd
 from package1 import module2
 from package1 import module3
+import pandas as pd
+from pandas.io.json import json_normalize
+import json
+import ast
 
 
 def report(results_merge_data_add_info):
-
     reports = []
     report1 = pd.DataFrame(results_merge_data_add_info.groupby('Sector')['Name'].count().sort_values(ascending=False))
     report1.rename(columns={'Name': 'Count'}, inplace=True)
@@ -17,20 +19,24 @@ def report(results_merge_data_add_info):
     reports.append('report2')
 
     report3 = pd.DataFrame(
-        results_merge_data_add_info.loc[results_merge_data_add_info['Age'] != -1, :].groupby('Sector')['Age', 'Worth'].agg(
+        results_merge_data_add_info.loc[results_merge_data_add_info['Age'] != -1, :].groupby('Sector')[
+            'Age', 'Worth'].agg(
             ['min', 'max', 'mean', 'median', 'std']))
     reports.append('report3')
 
-    mpg_labels = ['1- Younger: < 38', '2- A little less young: 38 < 53', '3- Moderate: 53 < 68', '4- A little old: 68 < 84',
+    mpg_labels = ['1- Younger: < 38', '2- A little less young: 38 < 53', '3- Moderate: 53 < 68',
+                  '4- A little old: 68 < 84',
                   '5- Older 84 < 99']
     report5 = pd.DataFrame(
-        pd.cut(results_merge_data_add_info.loc[results_merge_data_add_info['Age'] != -1, 'Age'], bins=5, labels=mpg_labels))
+        pd.cut(results_merge_data_add_info.loc[results_merge_data_add_info['Age'] != -1, 'Age'], bins=5,
+               labels=mpg_labels))
     report5 = pd.DataFrame(report5.groupby('Age')['Age'].count())
     report5.rename(columns={'Age': 'Age_group', 'Age': 'Age'}, inplace=True)
     report5['Percentage'] = round(report5.Age / sum(report5.Age) * 100, 1)
     reports.append('report5')
 
-    image5 = results_merge_data_add_info.loc[results_merge_data_add_info['Age'] != -1, 'Age'].hist(bins=5, figsize=(16, 8))
+    image5 = results_merge_data_add_info.loc[results_merge_data_add_info['Age'] != -1, 'Age'].hist(bins=5,
+                                                                                                   figsize=(16, 8))
     fig = image5.get_figure()
     fig.savefig('./reports/image5.png')
     reports.append('image5')
@@ -73,10 +79,77 @@ def report(results_merge_data_add_info):
         </style>
         """
 
+    table_stokes = results_merge_data_add_info[['Name', 'FinancialAssets']].dropna()
+
+    Ls = []
+
+    def New_DF(Name, FinancialAssets):
+        test_string = '"' + FinancialAssets[1:-1] + '"'
+        try:
+            res = json.loads(test_string)
+            result_dict = ast.literal_eval(res)
+            if type(result_dict) == dict:
+                # Ls = [v for k, v in i.items() if k=='exchange']
+                for k, v in i.items():
+                    if k == 'exchange':
+                        exchange = v
+                    elif k == 'companyName':
+                        Ls.append(Name + " ~ " + v + " ~ " + exchange)
+            else:
+                # Ls = [v for i in result_dict for k, v in i.items() if k=='exchange']
+                for i in result_dict:
+                    for k, v in i.items():
+                        if k == 'exchange':
+                            exchange = v
+                        elif k == 'companyName':
+                            Ls.append(Name + " ~ " + v + " ~ " + exchange)
+        except:
+            return FinancialAssets
+        return list(set(Ls))
+
+    table_stokes.apply(lambda row: New_DF(row.Name, row.FinancialAssets), axis=1)
+
+    Ls = []
+
+    table_stokes['more_data'] = table_stokes.apply(lambda row: New_DF(row.Name, row.FinancialAssets), axis=1)
+
+    Ls2 = [i.split(' ~ ') for i in Ls]
+
+    df1 = pd.DataFrame(Ls2)
+    df1.drop_duplicates()
+    df1.rename(columns={0: 'Name', 1: 'CompanyName', 2: 'Exchange'}, inplace=True)
+
+    report7 = df1[['Name', 'CompanyName', 'Exchange']].drop_duplicates()
+    report7 = pd.DataFrame(report7.groupby(['CompanyName', 'Exchange'])['Name'].count().sort_values(ascending=False))
+    report7.rename(columns={'Name': 'Count'}, inplace=True)
+    report7['Percentage'] = round(report7.Count / sum(report7.Count) * 100, 1)
+    report7 = report7.reset_index().head(3)
+    report7.index += 1
+    report7 = report7[['CompanyName', 'Exchange']]
+    reports.append('report7')
+
+    report8 = df1[['Exchange', 'Name']].drop_duplicates()
+    report8 = pd.DataFrame(report8.groupby('Exchange')['Name'].count().sort_values(ascending=False))
+    report8.rename(columns={'Name': 'Count'}, inplace=True)
+    report8['Percentage'] = round(report8.Count / sum(report8.Count) * 100, 1)
+    report8 = report8.reset_index().head(3)
+    report8.index += 1
+    reports.append('report8')
+
+    report9 = df1[['Name', 'CompanyName']].drop_duplicates()
+    report9 = pd.DataFrame(report9.groupby('Name')['CompanyName'].count().sort_values(ascending=False))
+    report9.rename(columns={'CompanyName': 'Count'}, inplace=True)
+    report9 = report9.reset_index().head(3)
+    report9.index += 1
+    reports.append('report9')
+
     module2.dataframe_to_image(report1, css, './reports/report1.png')
     module2.dataframe_to_image(report2, css, './reports/report2.png')
     module2.dataframe_to_image(report3, css, './reports/report3.png')
     module2.dataframe_to_image(report5, css, './reports/report5.png')
     module2.dataframe_to_image(report6, css, './reports/report6.png')
+    module2.dataframe_to_image(report7, css, './reports/report7.png')
+    module2.dataframe_to_image(report8, css, './reports/report8.png')
+    module2.dataframe_to_image(report9, css, './reports/report9.png')
 
     module3.to_pdf(reports)
